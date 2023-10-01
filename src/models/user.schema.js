c48451b83b39048cd6965b7bf3bf2a6f279b9ab2
Av3001 <113854539+Av3001@users.noreply.github.com>
2023-10-01 16:33:34 +0000

    bcrypt functionality

--- src/models/user.schema.js
+++ src/models/user.schema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import AuthRoles from "../utils/authRoles.js";
+import bcrpyt from "bcryptjs";
 
 const userSchema=mongoose.Schema({
     name:{
@@ -25,6 +26,19 @@ const userSchema=mongoose.Schema({
     forgotPasswordToken:String,
     forgotPasswordExpiry:Date,
 },{timestamps : true})
+
+userSchema.pre("save",async function(next){
+    if (!this.isModified("password")) return next()
+    this.password=await bcrpyt.hash(this.password,10);
+    next()  
+})
+
+userSchema.methods={
+    comparePassword: async function(enteredPassword){
+        return await bcrpyt.compare(enteredPassword,this.password)
+    }
+}
+
 
 
 export default mongoose.model('User',userSchema); 
