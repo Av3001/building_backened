e4d3978a4755960dd31ddd3b24bbcd679d2689a8
Av3001 <113854539+Av3001@users.noreply.github.com>
2023-10-01 18:51:02 +0000

    user logged in and out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -42,3 +42,42 @@ export const signup=asyncHandler(async(req,res)=>{
         user,
     })
 })
+
+export const login=asyncHandler(async (req,res)=>{
+    const {email,password}=req.body
+    
+    //validation
+    if(!email || !password){
+        throw new CustomError("Please fill all details",400)
+
+    }
+    const user=User.findOne({email}).select("+password")
+
+    if(!user){
+        throw new CustomError("Invalid credentials",400)
+    }
+    const isPasswordMatched=await user.comparePassword(password)
+    if(isPasswordMatched){
+        const token=user.getJWTtoken()
+        user.password=undefined
+        res.cookie("token",token,cookieOptions)
+        return res.status(200).json({
+            success:true,
+            token,
+            user
+        })
+    }
+    throw new CustomError("password is incorrect",400)
+})
+
+export const logout=asyncHandler(async (req,res)=>{
+    res.cookie("token",null,{
+        expires:new Date(Date.now()),
+        httpOnly:true
+    })
+
+    res.status(200).json({
+        success:true,
+        message:"Logged Out"
+    })
+} )
