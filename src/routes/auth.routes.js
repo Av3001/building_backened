a9c0d1d1451aab11528e469b0116ece7bffb0318
Av3001 <113854539+Av3001@users.noreply.github.com>
2023-10-05 17:45:03 +0000

    discussion on routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express";
-import {signUp,login, getProfile} from "../controllers/auth.controller.js"
+import {signUp,login, getProfile, logout, forgotPassword,resetPassword} from "../controllers/auth.controller.js"
 import {isLoggedIn} from "../middlewares/auth.middleware.js"
 
 
@@ -7,5 +7,10 @@ const router=Router()
 
 router.post("/signup",signUp)
 router.post("/login",login)
+router.get("/logout",logout)
+router.post("/password/forgot/",forgotPassword)
+router.post("/password/reset/:token",resetPassword)
 router.post("/profile",isLoggedIn,getProfile)
 
+export default router;
+ 
